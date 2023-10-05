8935edcac24752e891d7ae72c1d67e8fb06f923e
Koschei <nitianzero@gmail.com>
2023-10-05 22:52:10 +0800

    feat: use deep and light copies

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -1,5 +1,7 @@
+import geo.Circle;
+import geo.Rectangle;
+
 import java.util.Date;
-import geo.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +12,15 @@ public class Main {
         p1.print(p3);
         p2.print(new Rectangle(2, 5));
         p3.print(new Circle(3, "yellow"));
-        p1.print(new MyLogger(new Date(), "Hello Koschei"));
+        p1.print(new MyLogger(new Date(), "Hello Koschei."));
+
+        MyLogger myLogger = new MyLogger();
+        MyLogger myLoggerCloned = myLogger.clone();
+        p1.print(myLogger);
+        p1.print(myLoggerCloned);
+
+        // String为引用对象，浅拷贝中为true，深拷贝中为false
+        System.out.println(myLogger.getMsg() == myLoggerCloned.getMsg());
+        System.out.println(myLogger.getMsg().equals(myLoggerCloned.getMsg()));
     }
 }
--- lab2/src/MyLogger.java
+++ lab2/src/MyLogger.java
@@ -1,14 +1,14 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MyLogger {
+public class MyLogger implements Cloneable {
 
-    private final Date logTime_;
-    private final String msg_;
+    private Date logTime_;
+    private String msg_;
 
     public MyLogger() {
         logTime_ = new Date();
-        msg_ = "";
+        msg_ = "Default message.";
     }
 
     public MyLogger(Date logTime, String msg) {
@@ -25,11 +25,33 @@ public class MyLogger {
         return msg_;
     }
 
+    public Date setLogTime(Date logTime) {
+        return logTime_ = logTime;
+    }
+
+    public void setMsg(String msg_) {
+        this.msg_ = msg_;
+    }
+
     @Override
     public String toString() {
         return "MyLogger [" +
                 "LogTime=" + getLogTime() +
                 ", msg=" + getMsg() +
                 "]";
+    }
+
+    @Override
+    public MyLogger clone() {
+        try {
+            // 深拷贝 TODO: copy mutable state here, so the clone can't change the internals of the original
+//            MyLogger clone = (MyLogger) super.clone();
+//            clone.msg_ = new String(msg_);
+//            return clone;
+            // 浅拷贝
+            return (MyLogger) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
     }
 }
